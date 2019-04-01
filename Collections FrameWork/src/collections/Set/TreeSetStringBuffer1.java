0a1580476a6a860bff5377c10803f8d4d6a05479
raina <raina@raina-Lenovo-ideapad-330-15IKB>
2019-04-01 12:21:42 +0530

    comparator

--- Collections FrameWork/src/collections/Set/TreeSetStringBuffer1.java
+++ Collections FrameWork/src/collections/Set/TreeSetStringBuffer1.java
@@ -1,8 +1,8 @@
 package collections.Set;
+//special case of StringBuffer
+import java.util.*;
 
-import java.util.TreeSet;
-
-public class TreeSetDemo2 {
+public class TreeSetStringBuffer1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,7 +11,7 @@ public class TreeSetDemo2 {
         t.add(new StringBuffer("a"));
         t.add(new StringBuffer("Z"));
         t.add(new StringBuffer("L"));
-       // everything is fine but we will get CCE because StringBuffer is not comparable hence DNSO will not be followed
+       // everything is fine but we will get RTE:CCE because StringBuffer is not comparable hence DNSO will not be followed
         System.out.println(t);
 	}
 
---
+++ Collections FrameWork/src/sortingtechniques/ComparableDemo.java
@@ -0,0 +1,18 @@
+package sortingtechniques;
+
+public class ComparableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//compareto gives -ve element is greater
+		//compareto gives +ve element is smaller
+		//compareto gives 0 for duplicates
+		
+        System.out.println("A".compareTo("Z"));
+        System.out.println("K".compareTo("A"));
+        System.out.println("A".compareTo("A"));
+        System.out.println("A".compareTo(null));
+	}
+
+}
+ 
